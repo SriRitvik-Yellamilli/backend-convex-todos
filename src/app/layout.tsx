a2f8d132f7366e23db1539b6e187d482cd0192c0
SriRitvik Yellamilli <sriritvik.yellamilli@gmail.com>
2024-10-24 21:59:21 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: "Get Things Done!",
+  title: "Todos AI!",
   description: "Boost your productivity with Get Things Done! Stay organized, track your tasks, and accomplish more with our intuitive and user-friendly app. Whether you're managing daily chores or long-term projects, Get Things Done empowers you to prioritize, plan, and execute with ease. Unlock your full potential today and take control of your to-do list!",
 };
 

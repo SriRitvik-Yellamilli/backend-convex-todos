c6a639feb57ede1bc2ee11100419e344a3889004
SriRitvik Yellamilli <sriritvik.yellamilli@gmail.com>
2024-10-24 22:01:51 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Todos AI!",
-  description: "Boost your productivity with Todos AI! Stay organized, track your tasks, and accomplish more with our intuitive and user-friendly app. Whether you're managing daily chores or long-term projects, Get Things Done empowers you to prioritize, plan, and execute with ease. Unlock your full potential today and take control of your to-do list!",
+  description: "Boost your productivity with Todos AI! Stay organized, track your tasks, and accomplish more with our intuitive and user-friendly app. Whether you're managing daily chores or long-term projects, Todos AI empowers you to prioritize, plan, and execute with ease. Unlock your full potential today and take control of your to-do list!",
 };
 
 export default function RootLayout({
